558484348aeb39403908f61998727ece213052b7
Mahmoud Wael <Mahmoud Wael@DESKTOP-UTQ37J8>
2024-10-05 23:15:21 +0300

    Add project files.

---
+++ Handle-duplication/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Handle_duplication
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                
+                Console.WriteLine("Enter a list of integers separated by spaces:");
+                string integers = Console.ReadLine();
+
+                
+                string[] inputArray = integers.Split(' ');
+
+                
+                List<int> numbers = new List<int>();
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    numbers.Add(int.Parse(inputArray[i]));
+                }
+
+               
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    for (int j = i + 1; j < numbers.Count; j++)
+                    {
+                        if (numbers[i] == numbers[j])
+                        {
+                           
+                            throw new Exception($"Duplicate number founded: {numbers[i]}");
+                        }
+                    }
+                }
+
+                
+                Console.WriteLine("  entered numbers are:");
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Please enter only integers.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
